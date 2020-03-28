d8d2ec74992f2867ba06c678bfcf12dfb65817d9
藤田優作 <hinasakun@outlook.jp>
2020-03-29 07:00:14 +0900

    Initial Commit

---
+++ SnowCatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowCatch
+//
+//  Created by 藤田優作 on 2020/03/29.
+//  Copyright © 2020 藤田優作. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
